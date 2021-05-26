c1cec52bd57fee16948f4045f5db974a24815cea
Jorge Bernal <jbernal@gmail.com>
2021-05-26 16:20:55 +0200

    Sample app

--- ChaChing/ChaChingApp.swift
+++ ChaChing/ChaChingApp.swift
@@ -6,6 +6,8 @@
 //
 
 import SwiftUI
+import AudioToolbox
+import AVFoundation
 
 @main
 struct ChaChingApp: App {
@@ -13,5 +15,50 @@ struct ChaChingApp: App {
         WindowGroup {
             ContentView()
         }
+    }
+}
+
+let soundURL = Bundle.main.url(forResource: "o", withExtension: "caf")!
+
+struct AudioServicesPlayer {
+    static let shared: Self = .init()
+
+    private var soundID: SystemSoundID = 0
+
+    init() {
+        AudioServicesCreateSystemSoundID(soundURL as CFURL, &soundID)
+    }
+
+    func play() {
+        AudioServicesPlaySystemSound(soundID)
+    }
+}
+
+struct AVFoundationPlayer {
+    static let shared: Self = .init()
+
+    let player = try! AVAudioPlayer(contentsOf: soundURL)
+
+    func play() {
+        player.play()
+    }
+}
+
+struct ContentView: View {
+    var body: some View {
+        VStack(spacing: 20) {
+            Button("Play using Audio Services") {
+                AudioServicesPlayer.shared.play()
+            }
+            Button("Play using AVFoundation") {
+                AVFoundationPlayer.shared.play()
+            }
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
     }
 }
