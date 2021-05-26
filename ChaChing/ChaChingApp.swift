36152291c0b83d864436e44c972f42eb91beb568
Jorge Bernal <jbernal@gmail.com>
2021-05-26 16:01:12 +0200

    Initial Commit

---
+++ ChaChing/ChaChingApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChaChingApp.swift
+//  ChaChing
+//
+//  Created by Jorge Bernal on 26/5/21.
+//
+
+import SwiftUI
+
+@main
+struct ChaChingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChaChing/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChaChing
+//
+//  Created by Jorge Bernal on 26/5/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
